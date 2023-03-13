99c434f0d0c951a0fe487e3daa5911681c063d21
sekkiyah <seth_hinshaw@hotmail.com>
2023-03-12 19:45:26 -0500

    added exposure to company id

---
+++ src/main/java/sekk/apibuilder/configs/RestConfiguration.java
@@ -0,0 +1,18 @@
+package sekk.apibuilder.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import sekk.apibuilder.models.Company;
+
+@Configuration
+public class RestConfiguration implements RepositoryRestConfigurer {
+  
+  @Override
+  public void configureRepositoryRestConfiguration(
+    RepositoryRestConfiguration config, CorsRegistry cors) {
+      config.exposeIdsFor(Company.class);
+    }
+}
